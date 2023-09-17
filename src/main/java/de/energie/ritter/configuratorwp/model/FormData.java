98fa6b07fc63fede8de37570099959faeca22d24
andrejpotryvaj <potryvay.andrey99@gmail.com>
2023-09-17 13:55:54 +0200

    Form input structure created

--- src/main/java/de/energie/ritter/configuratorwp/model/FormData.java
+++ src/main/java/de/energie/ritter/configuratorwp/model/FormData.java
@@ -2,5 +2,17 @@ package de.energie.ritter.configuratorwp.model;
 
 public record FormData(
         int heatLoad,
-        String isEblan
+        String eheater,
+        String calcpos,
+        String checkplace,
+        String newplace,
+        String distance,
+        String newdistance,
+        String VBoption,
+        String acoustic,
+        String newplaceVA,
+        String radtemp,
+        String reconstradtemp,
+        String surfencradtemp,
+        String woodcauldron
 ){}
--- src/main/java/de/energie/ritter/configuratorwp/service/impl/WordDocService.java
+++ src/main/java/de/energie/ritter/configuratorwp/service/impl/WordDocService.java
@@ -1,6 +1,8 @@
 package de.energie.ritter.configuratorwp.service.impl;
 
+import de.energie.ritter.configuratorwp.model.FormData;
 import de.energie.ritter.configuratorwp.service.DocService;
+import org.springframework.stereotype.Service;
 
 public class WordDocService implements DocService {
 }
