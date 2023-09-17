88c8c73cf08b5a0a0084af7d266dff6b567a2eef
andrejpotryvaj <potryvay.andrey99@gmail.com>
2023-09-17 13:56:08 +0200

    Form input structure created

--- src/main/java/de/energie/ritter/configuratorwp/service/DocService.java
+++ src/main/java/de/energie/ritter/configuratorwp/service/DocService.java
@@ -1,4 +1,7 @@
 package de.energie.ritter.configuratorwp.service;
 
+import de.energie.ritter.configuratorwp.model.FormData;
+
 public interface DocService {
+    String generateOffer(FormData formData);
 }
--- src/main/java/de/energie/ritter/configuratorwp/service/impl/WordDocService.java
+++ src/main/java/de/energie/ritter/configuratorwp/service/impl/WordDocService.java
@@ -4,5 +4,11 @@ import de.energie.ritter.configuratorwp.model.FormData;
 import de.energie.ritter.configuratorwp.service.DocService;
 import org.springframework.stereotype.Service;
 
+@Service
 public class WordDocService implements DocService {
+
+    @Override
+    public String generateOffer(FormData formData) {
+        return null;
+    }
 }
