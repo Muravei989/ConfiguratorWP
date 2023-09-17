3b6dae7be3e16eb11c7f1dbd3141b3a3727ac111
Vladyslav_Tsiupa <Vladyslav.Tsiupa@sephora.com>
2023-09-17 15:35:07 +0300

    SERVAK

--- src/main/java/de/energie/ritter/configuratorwp/controller/FormController.java
+++ src/main/java/de/energie/ritter/configuratorwp/controller/FormController.java
@@ -2,6 +2,8 @@ package de.energie.ritter.configuratorwp.controller;
 
 
 import de.energie.ritter.configuratorwp.model.FormData;
+import de.energie.ritter.configuratorwp.service.DocService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +13,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class FormController {
 
+    @Autowired
+    DocService doc;
+
     @GetMapping("/form-page")
     public String initialPage() {return "form-page";}
 
     @PostMapping("/submit")
     public String handleForm(@ModelAttribute("formData") FormData formData, ModelMap model) {
-        model.addAttribute("heatload", formData.heatLoad());
-        model.addAttribute("eblan", formData.isEblan());
+        String offerLink = doc.generateOffer(formData);
+        model.addAttribute("fileLink", offerLink);
         return "result";
     }
 }
--- src/main/java/de/energie/ritter/configuratorwp/service/impl/WordDocService.java
+++ src/main/java/de/energie/ritter/configuratorwp/service/impl/WordDocService.java
@@ -2,13 +2,53 @@ package de.energie.ritter.configuratorwp.service.impl;
 
 import de.energie.ritter.configuratorwp.model.FormData;
 import de.energie.ritter.configuratorwp.service.DocService;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 @Service
 public class WordDocService implements DocService {
+    @Value("${templatePath}")
+    private String templatePath;
 
     @Override
     public String generateOffer(FormData formData) {
-        return null;
+        String filePath = getClass().getClassLoader().getResource(templatePath).getPath();
+        String newFilePath = filePath.replace("TestTemplate", "Result" + UUID.randomUUID());
+        try (
+                InputStream in = new FileInputStream(filePath);
+                FileOutputStream out = new FileOutputStream(newFilePath);
+                XWPFDocument doc = new XWPFDocument(in);
+        ) {
+            doc
+                    .getParagraphs()
+                    .forEach(p -> {
+                        p
+                                .getRuns()
+                                .forEach(r -> {
+                                    String text = r.getText(0);
+                                    int hl = formData.heatLoad();
+                                    if (text.equals("Heat Load {{Value1}}")) {
+                                        String updatedRunText = text.replace("{{Value1}}", String.valueOf(hl));
+                                        r.setText(updatedRunText, 0);
+                                    } else if (text.equals("Price {{Value2}}")) {
+                                        String updatedRunText = text.replace("{{Value2}}", hl > 10 ? "Many" : "Not Many");
+                                        r.setText(updatedRunText, 0);
+                                    }
+                                });
+                    });
+
+            doc.write(out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return newFilePath;
     }
 }
