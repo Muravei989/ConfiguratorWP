f82c30b42218cc7c1d019975c37b8ba1721fe310
andrejpotryvaj <potryvay.andrey99@gmail.com>
2023-09-17 14:22:25 +0200

    Form input structure created

--- src/main/java/de/energie/ritter/configuratorwp/model/FormData.java
+++ src/main/java/de/energie/ritter/configuratorwp/model/FormData.java
@@ -1,7 +1,10 @@
 package de.energie.ritter.configuratorwp.model;
 
+import java.util.SplittableRandom;
+
 public record FormData(
         int heatLoad,
+        int distancetoboard,
         String eheater,
         String calcpos,
         String checkplace,
@@ -14,5 +17,19 @@ public record FormData(
         String radtemp,
         String reconstradtemp,
         String surfencradtemp,
-        String woodcauldron
+        String woodcauldron,
+        String heatload8,
+        String heatload910,
+        String heatload11,
+        String heatload12ja,
+        String heatload12ne,
+        String heatload13,
+        String heatload14ja,
+        String heatload14ne,
+        String heatload15,
+        String heatload16,
+        String heatload1725,
+        String heatload2650
+
+
 ){}
