8ed2f4d5a722cf5641194b01446690ae214a81b6
andrejpotryvaj <potryvay.andrey99@gmail.com>
2023-09-18 16:38:09 +0200

    Form input structure created

--- src/main/java/de/energie/ritter/configuratorwp/service/impl/TextDocService.java
+++ src/main/java/de/energie/ritter/configuratorwp/service/impl/TextDocService.java
@@ -34,8 +34,32 @@ public class TextDocService implements DocService {
 
         String templateText = Files.readAllLines(Paths.get(sourcePath)).stream().collect(Collectors.joining(System.lineSeparator()));
         String resultContent = templateText
-                .replace("{{t1}}", String.valueOf(formData.getHeatLoad()))
-                .replace("{{t2}}", String.valueOf(formData.getDistancetoboard()));
+                .replace("{{heatload}}", String.valueOf(formData.getHeatLoad()))
+                .replace("{{distancetoboard}}", String.valueOf(formData.getDistancetoboard()))
+                .replace("{{eheater}}", formData.getEheater())
+                .replace("{{calcpos}}", formData.getCalcpos())
+                .replace("{{checkplace}}", formData.getCheckplace())
+                .replace("{{newplace}}", formData.getNewplace())
+                .replace("{{distance}}", formData.getDistance())
+                .replace("{{newdistance}}", formData.getNewdistance())
+                .replace("{{VBoption}}", formData.getVBoption())
+                .replace("{{acoustic}}", formData.getAcoustic())
+                .replace("{{radtemp}}", formData.getRadtemp())
+                .replace("{{reconstradtemp}}", formData.getReconstradtemp())
+                .replace("{{surfencradtemp}}", formData.getSurfencradtemp())
+                .replace("{{woodcauldron}}", formData.getWoodcauldron())
+                .replace("{{heatload8}}", formData.getHeatload8())
+                .replace("{{heatload910}}", formData.getHeatload910())
+                .replace("{{heatload11}}", formData.getHeatload11())
+                .replace("{{heatload12ja}}", formData.getHeatload12ja())
+                .replace("{{heatload12ne}}", formData.getHeatload12ne())
+                .replace("{{heatload13}}", formData.getHeatload13())
+                .replace("{{heatload14ja}}", formData.getHeatload14ja())
+                .replace("{{heatload14ne}}", formData.getHeatload14ne())
+                .replace("{{heatload15}}", formData.getHeatload15())
+                .replace("{{heatload16}}", formData.getHeatload16())
+                .replace("{{heatload1725}}", formData.getHeatload1725())
+                .replace("{{heatload2650}}", formData.getHeatload2650());
 
         Files.writeString(Paths.get(resultPath), resultContent);
         return newFileName;
