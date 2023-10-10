6b176a45af6de84e7778e02899a8c7080d466f9e
Vladyslav_Tsiupa <Vladyslav.Tsiupa@sephora.com>
2023-10-10 15:37:03 +0300

    test

--- src/main/resources/static/assets/js/form-page.js
+++ src/main/resources/static/assets/js/form-page.js
@@ -1,82 +1,35 @@
-let currentState;
+let hl8State = ["heatload8Box", "checkplaceBox", "newplaceBox", "distanceBox", "newdistanceBox", "acousticBox", "newplaceVABox", "radtempBox", "reconstradtempBox", "surfencradtempBox"];
+// let hl910State = new State("heatload910", ["heatload910Box", "checkplaceBox", "newplaceBox hidden", "distanceBox hidden", ]);
+// let hl11State = new State("heatload11", ["heatload11Box"]);
+// add all states here
+$("#heatLoad").change(function(){
+    $(".heatload8Box").removeClass("hidden");
+});
 
-document.addEventListener('DOMContentLoaded', function() {
-
-class State {
-    name = "";
-    inputs = [];
-    step = 0;
-
-    constructor(name, inputs) {
-        this.name = name;
-        this.inputs = inputs;
-    }
-
-    incrementStep() {
-        this.step++;
-    }
-
-    dropStep() {
-        this.step = 0;
-    }
-
-    illuminate() {
-        for (let i = 0; i < this.inputs.length; i++) {
-            illuminateInput(this.inputs[i]);
-        }
-    }
-
-    hide() {
-        for (let i = 0; i < this.inputs.length; i++) {
-            hideInput(this.inputs[i]);
-        }
-    }
-}
-
-let hl8State = new State("heatload8", ["heatload8Box", "checkplaceBox", "newplaceBox", "distanceBox", "newdistanceBox", "acousticBox", "newplaceVABox", "radtempBox", "reconstradtempBox", "surfencradtempBox","pipeLenghtBox" ]);
-let hl910State = new State("heatload910", ["heatload910Box", "checkplaceBox", "newplaceBox hidden", "distanceBox hidden", ]);
-let hl11State = new State("heatload11", ["heatload11Box"]);
-
-let states = [hl8State, hl910State];
-
-currentState = hl8State;
-
-
-function hideInput(id) {
-    document.getElementById(id).classList.remove("hidden");
-}
-
-function illuminateInput(id) {
-    document.getElementById(id).classList.add("hidden");
-}
-
-function resetInactive() {
-    for (const state1 of states) {
-        if (!(state1.name === currentState.name)) {
-            state1.hide();
-            state1.dropStep();
-        }
-    }
-}
-
-function heatLoadToggle() {
-    let val = parseInt(document.getElementById("heatLoad").value);
-    if (val === 8) {
-        currentState = hl8State;
-    } else if (val === 9 || val === 10) {
-        currentState = hl910State;
-    }
-    resetInactive();
-    currentState.incrementStep();
-    currentState.illuminate();
-}
-
-
-function heatload8BoxToggle() {
-    resetInactive();
-    currentState.incrementStep();
-    currentState.illuminate();
-}
-
-document.getElementById("heatload8Box").addEventListener('change', heatload8BoxToggle())
+$("#heatload8").change(function(){
+    $(".checkplaceBox").removeClass("hidden");
+});
+$("#checkplace").change(function(){
+    $(".newplaceBox").removeClass("hidden");
+});
+$("#newplace").change(function(){
+    $(".distanceBox").removeClass("hidden");
+});
+$("#distance").change(function(){
+    $(".newdistanceBox").removeClass("hidden");
+});
+$("#newdistance").change(function(){
+    $(".acousticBox").removeClass("hidden");
+});
+$("#acoustic").change(function(){
+    $(".newplaceVABox").removeClass("hidden");
+});
+$("#newplaceVA").change(function(){
+    $(".radtempBox").removeClass("hidden");
+});
+$("#radtemp").change(function(){
+    $(".reconstradtempBox").removeClass("hidden");
+});
+$("#reconstradtemp").change(function(){
+    $(".surfencradtempBox").removeClass("hidden");
 });
